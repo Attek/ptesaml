f51a3de50d27e6085a85d679fad0e44491d9613b
attek <pest.attila@gmail.com>
2016-09-19 14:24:17 +0200

    test

---
+++ src/Saml.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: attek
+ * Date: 2016/09/19
+ * Time: 14:05
+ */
+
+namespace ptesaml;
+
+
+class Saml extends Object
+{
+    /**
+     * Path to simplesamlphp on server
+     * @var string
+     */
+    public $simpleSamlPath = "/usr/share/simplesamlphp/";
+
+    public static function test()
+    {
+        echo "Test";
+    }
+
+}
