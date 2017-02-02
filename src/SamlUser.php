c6a085ac2ce5d123b1124f2aa1a6f717745cf684
attek <pest.attila@gmail.com>
2017-02-02 10:44:01 +0100

    DISPLAY NAME,SN, GIVEN NAME

--- src/SamlUser.php
+++ src/SamlUser.php
@@ -10,9 +10,18 @@ class SamlUser
     const MAIL = "mail";
     const MAIL_ID = "urn:oid:0.9.2342.19200300.100.1.3";
 
+    const DISPLAY_NAME = "urn:oid:2.16.840.1.113730.3.1.241";
+    const SN = "urn:oid:2.5.4.4";
+    const GIVEN_NAME = "urn:oid:2.5.4.42";
+
+
     public $locale_id;
     public $scope;
     public $mail_list;
+    public $eduPersonPrincipalName;
+    public $displayName;
+    public $sn;
+    public $givenName;
 
     public function __construct($attributes = [])
     {
@@ -25,6 +34,18 @@ class SamlUser
                     $data = explode("@", $attribute[0]);
                     $this->locale_id = $data[0];
                     $this->scope = $data[1];
+                }
+
+                if ($key == self::DISPLAY_NAME) {
+                    $this->displayName = $attribute;
+                }
+
+                if ($key == self::SN) {
+                    $this->sn = $attribute;
+                }
+
+                if ($key == self::GIVEN_NAME) {
+                    $this->givenName = $attribute;
                 }
 
                 if ($key == self::MAIL || $key == self::MAIL_ID) {
